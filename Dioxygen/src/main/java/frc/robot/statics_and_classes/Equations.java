7b718a6ed42c6151037f1c09f35f67f388c67d5e
Lucas Brunner <threemotors@gmail.com>
2019-06-18 19:20:48 -0400

    Added descriptions to the methods in the "Equations" class.

--- Dioxygen/src/main/java/frc/robot/statics_and_classes/Equations.java
+++ Dioxygen/src/main/java/frc/robot/statics_and_classes/Equations.java
@@ -5,6 +5,14 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class Equations {
 
+    /**
+     * If the input is within a given amount of 0, the output is set to 0.
+     * 
+     * @param number
+     * @param deadzone If the input is within this amount of 0 the output will be 0.
+     * 
+     * @return The input with a deadzone applied.
+     */
     public static double deadzone(double number, double deadzone) {
         if (Math.abs(number) > Math.abs(deadzone)) {
             return number;
@@ -13,10 +21,27 @@ public class Equations {
         }
     }
 
+
+    /**
+     * If the input is within 0.1 of 0, the output is set to 0.
+     * 
+     * @param number
+     * 
+     * @return The input with a 0.1 deadzone applied.
+     */
     public static double deadzone(double number) {
         return deadzone(number, 0.1);
     }
 
+    /**
+     * If the double input is outside the given range then it is reduced/increased to the max or min,
+     * 
+     * @param input
+     * @param min
+     * @param max
+     * 
+     * @return The clamped input.
+     */
     public static double clamp(double input, double min, double max) {
         double output = 0;
         if (input > max) {
@@ -29,6 +54,15 @@ public class Equations {
         return output;
     }
 
+    /**
+     * If the integer input is outside the given range then it is reduced/increased to the max or min,
+     * 
+     * @param input
+     * @param min
+     * @param max
+     * 
+     * @return The clamped input.
+     */
     public static int clamp(int input, int min, int max) {
         int output = 0;
         if (input > max) {
@@ -41,6 +75,15 @@ public class Equations {
         return output;
     }
 
+    /**
+     * Checks to see if the input is within the min/max range.
+     * 
+     * @param input
+     * @param min
+     * @param max
+     * 
+     * @return Whether the input is within the given range. (True/False)
+     */
     public static boolean insideRange(int input, int min, int max)
     {
         boolean output = false;
@@ -51,6 +94,15 @@ public class Equations {
         return output;
     }
 
+    /**
+     * If the input is outside the given range it is "wrapped" around by adding/subtracting the max value until it is within the given range.
+     * 
+     * @param input
+     * @param min
+     * @param max
+     * 
+     * @return The input wrapped to the given range.
+     */
     public static int wrap(int input, int min, int max)
     {
         min--;
@@ -85,24 +137,64 @@ public class Equations {
       }
     }
 
+    /**
+     * Always otputs a positive number, even if the given power is an odd number.
+     * 
+     * @param input
+     * @param power The exponential applied to input.
+     * 
+     * @return The absolute value of input^power.
+     */
     public static double exponetialAbs(double input, double power) {
         return Math.abs(Math.pow(input, power));
     }
 
+    /**
+     * If input is less than 0 the output will be negative, even if the given power is an even number.
+     * 
+     * @param input
+     * @param power The exponential applied to input.
+     * 
+     * @return input^power or -(input^power) if input < 0.
+     */
+    public static double exponentialNegativeBelowZero(double input, double power)
+    {
+        double output = Math.pow(input, power);
+        if (input < 0)
+        {
+            output = -output;
+        }
+        return output;
+    }
+
+    /**
+     * Returns a -1 to 1 range based of the position of the left and right triggers. 
+     * 
+     * @param controller
+     * 
+     * @return If the left trigger is down: left trigger value, if the right trigger is down: right trigger value, else 0.
+     */
     public static double triggersAsJoy(XboxController controller) {
         double leftTrig = deadzone(controller.getTriggerAxis(Hand.kLeft));
         double rightTrig = deadzone(controller.getTriggerAxis(Hand.kRight));
-        if (leftTrig == 0 && rightTrig == 0) {
-            return 0;
+        double output = 0;
+        if (leftTrig == 0 && rightTrig == 0) 
+        {
         } else if (leftTrig > 0) {
-            return -leftTrig;
+            output = -leftTrig;
         } else if (rightTrig > 0) {
-            return rightTrig;
-        } else {
-            return 0;
+            output = rightTrig;
         }
+        return output;
     }
 
+    /**
+     * Checks if the input is 0.
+     * 
+     * @param input
+     * 
+     * @return Whether the input is 0. (True/False)
+     */
     public static boolean isZero(double input) {
         if (input == Finals.zero) {
             return true;
